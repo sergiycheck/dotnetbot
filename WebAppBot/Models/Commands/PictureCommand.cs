d0e3360d116ed25a71358e99a6c4031297e14162
serhii <segiycheck@gmail.com>
2020-06-23 16:37:08 +0300

    working bot with images

--- WebAppBot/Models/Commands/PictureCommand.cs
+++ WebAppBot/Models/Commands/PictureCommand.cs
@@ -2,6 +2,7 @@
 using System.Collections.Generic;
 using System.IO;
 using System.Linq;
+using System.Net.Http;
 using System.Threading.Tasks;
 using Telegram.Bot;
 using Telegram.Bot.Types;
@@ -20,13 +21,24 @@ namespace WebAppBot.Models.Commands
             var chatId = message.Chat.Id;
             string path = "..\\..\\..\\..\\WebAppBot\\Resources\\honda-nsx_1.jpg";
             string absolutePath = @"E:\filesFromCDisk\TelegramBot\WebAppBot\Resources\honda-nsx_1.jpg";
-            string url2 = "https://wallpaperset.com/w/full/f/3/3/396056.jpg";
+            string url2 = "https://source.unsplash.com/random";
+            string onlyUrl = "https://source.unsplash.com";
+            var httpClient = new HttpClient();
+            httpClient.BaseAddress = new Uri(onlyUrl);
+            var response = await httpClient.GetAsync("/random");
+            response.EnsureSuccessStatusCode();
+            using (var responseStream = await response.Content.ReadAsStreamAsync())
+            {
+                InputOnlineFile randomPhoto = new InputOnlineFile(responseStream);
+                await Bot.BotClient.SendPhotoAsync(chatId, randomPhoto, " Random Photo from stream ");
+            }
+
             using (FileStream fileStream = File.Open(absolutePath,FileMode.Open))
             {
                 InputOnlineFile photo = new InputOnlineFile(fileStream);
                 photo.FileName = Path.GetFileName(absolutePath);
                 if(photo.Content!=null) await Bot.BotClient.SendPhotoAsync(chatId, photo, "Car from disk");
-                return await Bot.BotClient.SendPhotoAsync(chatId, url2, "Revolution!");
+                return await Bot.BotClient.SendPhotoAsync(chatId, url2, "Random photo!");
             }
 
 
